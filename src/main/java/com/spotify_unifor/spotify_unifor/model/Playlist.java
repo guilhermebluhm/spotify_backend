6f0d5f146bbe0838c7ae96811c12ea70befab3cb
Guilherme Saraiva Bluhm <80696919+guilhermebluhm@users.noreply.github.com>
2022-11-28 20:12:36 -0300

    include new field for add thumbnail image in music albums

--- src/main/java/com/spotify_unifor/spotify_unifor/model/Playlist.java
+++ src/main/java/com/spotify_unifor/spotify_unifor/model/Playlist.java
@@ -20,6 +20,8 @@ public class Playlist implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(unique = true)
+    private String thumbnail;
     @Column(length = 100, unique = true)
     private String nomePlaylist;
 
