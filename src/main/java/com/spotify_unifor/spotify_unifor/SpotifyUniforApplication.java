f00ab73e92f0dfd12cfb0858efe4ba605e19bd13
Guilherme Saraiva Bluhm <80696919+guilhermebluhm@users.noreply.github.com>
2022-11-29 10:33:49 -0300

    fix

--- src/main/java/com/spotify_unifor/spotify_unifor/SpotifyUniforApplication.java
+++ src/main/java/com/spotify_unifor/spotify_unifor/SpotifyUniforApplication.java
@@ -2,8 +2,9 @@ package com.spotify_unifor.spotify_unifor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class SpotifyUniforApplication {
 
 	public static void main(String[] args) {
