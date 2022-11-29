62f165ed7764d9dc39dd482158ffbc53243b9157
Guilherme Saraiva Bluhm <80696919+guilhermebluhm@users.noreply.github.com>
2022-11-29 10:00:42 -0300

    fix CORS-1

--- src/main/java/com/spotify_unifor/spotify_unifor/config/WebConfig.java
+++ src/main/java/com/spotify_unifor/spotify_unifor/config/WebConfig.java
@@ -11,6 +11,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
     }
 }
